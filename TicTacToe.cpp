c4f06b97d38dad297311459f66c2057f66762c5f
Joy Mace <Jocelyn.Mace@colorado.edu>
2020-02-14 10:03:46 -0700

    Edited CreateBoard() so that vector makes 3x3 matrix. DisplayBoard() displays matrix as board.

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 #include <vector>
 
-//void DisplayBoard();
+void DisplayBoard(vector<char> b);
 std::vector<char> CreateBoard();
 
 int main() {
     std::vector board = CreateBoard();
 }
 
-std::vector<char> CreateBoard(){
-    std::vector<char> b = {'_','_','_','_','_','_','_','_','_'};
+std::vector<char> CreateBoard()
+{
+    std::vector<char> b = {{'_','_','_'},
+						   {'_','_','_'},
+						   {'_','_','_'}};
     return b;
+}
+
+void DisplayBoard(vector<char> b)
+{
+	for (int i = 0; i < b.size(); i++)
+	{
+		for (int j = 0; j < b[i].size(); j++)
+		{
+			std::cout << b[i][j]; << " ";
+		}
+		std::cout << endl;
+	}
 }
