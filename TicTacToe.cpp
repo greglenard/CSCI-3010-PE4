006251b5f19eafaefd6670c376faf9bc41cef5dd
greglenard <greglenard@gmail.com>
2020-02-14 18:55:17 -0700

    added player choice

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -3,9 +3,20 @@
 
 std::vector<char> CreateBoard();
 void DisplayBoard(std::vector<char> board);
+int GetPlayerChoice();
 
 int main() {
+    int move;
     std::vector board = CreateBoard();
+    for(int i = 0; i < 9; i++){
+        DisplayBoard(board);
+        int move = GetPlayerChoice();
+        if (i%2==0){
+            board.at(move-1) = 'X';
+        } else {
+            board.at(move-1) = 'O';
+        }
+    }
 }
 
 std::vector<char> CreateBoard(){
@@ -23,4 +34,12 @@ void DisplayBoard(std::vector<char> board){
     std::cout << std::endl;
 }
 
+int GetPlayerChoice() {
+    std::cout << "left -> Right, Top -> bottom (1-9)" << std::endl;
+    std::cout << "Enter choice:  ";
+    int pc = 0;
+    std::cin >> pc;
+    std::cout << std::endl;
+    return pc;
+}
 
