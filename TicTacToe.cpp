f0213a5c94116d68388b413c5d2586f90d2bc38f
greglenard <greglenard@gmail.com>
2020-02-14 18:22:23 -0700

    person1 displayboard

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -3,6 +3,7 @@
 
 //void DisplayBoard();
 std::vector<char> CreateBoard();
+void DisplayBoard(std::vector<char> board);
 
 int main() {
     std::vector board = CreateBoard();
@@ -12,3 +13,15 @@ std::vector<char> CreateBoard(){
     std::vector<char> b = {'_','_','_','_','_','_','_','_','_'};
     return b;
 }
+
+void DisplayBoard(std::vector<char> board){
+    for (int i = 0; i < board.size(); i++){
+        std::cout << " " <<board.at(i) << " ";
+        if ((i+1)%3==0){
+            std::cout << std::endl;
+        }
+    }
+    std::cout << std::endl;
+}
+
+
